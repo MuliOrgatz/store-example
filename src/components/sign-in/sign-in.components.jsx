a716f5704d6e6ab6097b483381bf6536047a691a
MuliOrgatz <muli652@gmail.com>
2019-09-29 16:42:34 +0300

    finishing signin and signup

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.components';
 import Custombutton from '../custom-button/custom-button.components';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -15,12 +15,19 @@ class SignIn extends React.Component {
         }
     }
 
-    handelSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
-        this.setState={email:'', password:''}
+        const{email,password}= this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'',password:''});
+        }catch(error){
+            console.log(error);
+        }
     }
 
-    handelChange = event =>{
+    handleChange = event =>{
         const {value, name} = event.target;
         this.setState({[name]: value});
     }
@@ -31,17 +38,17 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handelSubmit}>
+                <form onSubmit={this.handleSubmit}>
                     <FormInput 
                     name="email" 
                     type="email"
                     label="email"
-                    handleChange={this.handelChange} 
+                    handleChange={this.handleChange} 
                     value={this.state.email} required/>
                     <FormInput 
                     name="password" 
                     type="password"
-                    handelChange={this.handelChange}
+                    handleChange={this.handleChange}
                     label= "password" 
                     value={this.state.password} 
                     required/>
